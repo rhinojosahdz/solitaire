3d96e62842ffc5adf93cef188e2989168f72a887
Hinojosa Hernandez <roberto.hinojosahernandez@one.verizon.com>
2017-02-20 23:31:39 -0600

    removed dead code

--- s.js
+++ s.js
@@ -2,15 +2,13 @@
  Property of Roberto Jaime Hinojosa Hernandez, All rights reserved.
  */
 
-// TODO que te diga cuando ganaste
+// TODO should tell you when you won
 
 (function ($) {
 
     var _this;
     var cards = ['s1', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 's9', 's10', 's11', 's12', 's13', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8', 'h9', 'h10', 'h11', 'h12', 'h13', 'd1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8', 'd9', 'd10', 'd11', 'd12', 'd13', 'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9', 'c10', 'c11', 'c12', 'c13'];
     var randomized_cards = [];
-    // var path = '';
-    // var window.cards_path = path + 'imgs/cards/';
     var drag_began = false;
     var card_position_before_drag;
     var number_of_tableau_cards = 0;
